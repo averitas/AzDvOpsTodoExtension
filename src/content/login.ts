9d3bc657d80d782946734d2bce45994134b912bd
Sicheng Liu <sichengliu@microsoft.com>
2024-03-11 15:54:35 +0800

    add login logic

--- src/content/login.ts
+++ src/content/login.ts
@@ -1,15 +1,23 @@
-console.log('content-script.ts')
+import { Configuration } from "@azure/msal-browser";
 
-const lgoinButton = document.createElement('button')
+export const MsalConfig: Configuration = {
+  auth: {
+    clientId: '84771574-3cfe-442d-964f-c9fc3db201ce',
+    authority: 'https://login.microsoftonline.com/common',
+    redirectUri: chrome.runtime.getURL('pages/popup/index.html'),
+  }
+};
 
-lgoinButton.addEventListener('click', function() {
-    const clientId = 'your-client-id';
-    const redirectUri = chrome.identity.getRedirectURL('provider_cb');
+export const ChromeLogin = function(callback: (token: string) => any) {
+    const clientId = MsalConfig.auth.clientId;
+    const redirectUri = chrome.identity.getRedirectURL();
+    console.log("redirect page is " + redirectUri);
+
     const authUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?' +
       'client_id=' + clientId +
       '&response_type=token' +
       '&redirect_uri=' + encodeURIComponent(redirectUri) +
-      '&scope=' + encodeURIComponent('https://graph.microsoft.com/.default');
+      '&scope=' + encodeURIComponent('Tasks.ReadWrite,User.Read');
   
     chrome.identity.launchWebAuthFlow({url: authUrl, interactive: true}, function(responseUrl) {
       if (chrome.runtime.lastError) {
@@ -24,12 +32,10 @@ lgoinButton.addEventListener('click', function() {
       console.log("Get token is :" + String(token))
   
       // Store token
-      chrome.storage.sync.set({token: token}, function() {
-        if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError.message);
-        } else {
-            console.log('Token saved');
-        }
-      });
-    });
-  });
+      callback(token!);
+    })
+};
+
+export const ChromeLogout = function(token: string) {
+  chrome.identity.removeCachedAuthToken({token: token});
+}
--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,12 +1,69 @@
 import React from 'react'
+import { PublicClientApplication } from '@azure/msal-browser';
+import { ChromeLogin, ChromeLogout, MsalConfig } from '../../content/login';
 
 const App = (): JSX.Element => {
+  const [token, setToken] = React.useState<string | null>(null);
+  const msalInstance = new PublicClientApplication(MsalConfig);
+
+  React.useEffect(() => {
+    // Retrieve token from chrome storage
+    chrome.storage.local.get(['token'], (result) => {
+      setToken(result.token);
+    });
+  }, []);
+
+  const saveTokenToStorage = function(token: string | null) {
+    chrome.storage.local.set({token: token}, function() {
+      if (chrome.runtime.lastError) {
+          console.log(chrome.runtime.lastError.message);
+      } else {
+          console.log('Token saved');
+      }
+    });
+    setToken(token);
+  }
+
+  const handleLogin = async () => {
+    await msalInstance.initialize();
+    try {
+      ChromeLogin(function(token: string) {
+        saveTokenToStorage(token)
+        setToken(token);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = async () => {
+    await msalInstance.initialize();
+    try {
+      ChromeLogout(token!);
+      setToken(null);
+
+      // Store token
+      saveTokenToStorage(null)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Popup Page</h1>
+      <h1>Extension Setting</h1>
       <p>If you are seeing this, React is working!</p>
+      {token ? (
+        <div>
+          <p>You have login successfully.</p>
+          <p>Token: {token}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
